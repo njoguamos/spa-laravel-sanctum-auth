10cef0fbfa70604600bb628eebcf209a08c44d0d
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2024-02-09 20:49:00 +0300

    Add laravel login endpoint

--- laravel-api/routes/web.php
+++ laravel-api/routes/web.php
@@ -18,3 +18,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use Illuminate\Http\Request;
+
+Route::middleware(['guest','throttle:api'])
+    ->post('/login', function (Request $request) {
+    // Validate the login request
+    $request->validate([
+        'email'    => [ 'required' ,'email' ],
+        'password' => [ 'required' ],
+    ]);
+
+    // Attempt to authenticate the user
+    if (Auth::attempt($request->only('email', 'password'))) {
+        // Authentication successful, redirect to intended page
+            return $request->user()->only(['name','email']);
+    }
+
+    return response()->json(['errors' => [
+        'email' => 'The provided credentials do not match our records.',
+    ]], 422);
+});
