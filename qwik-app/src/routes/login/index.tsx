4ef0b330d88cbd0f93a9d960cd664a7aaab4d60b
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2024-02-10 17:56:11 +0300

    Add useNavigate

--- qwik-app/src/routes/login/index.tsx
+++ qwik-app/src/routes/login/index.tsx
@@ -1,21 +1,24 @@
-import { component$ } from "@builder.io/qwik"
-import type { Input} from "valibot"
-import {email, maxLength, minLength, object, string} from "valibot"
+import { component$ } from "@builder.io/qwik";
+import type { Input } from "valibot";
+import { email, maxLength, minLength, object, string } from "valibot";
+import { useNavigate } from "@builder.io/qwik-city";
 
 // noinspection JSUnresolvedReference
-const endpoint = import.meta.env.PUBLIC_API_ENDPOINT
+const endpoint = import.meta.env.PUBLIC_API_ENDPOINT;
 
 const LoginSchema = object({
   email: string([
-    minLength(1, 'Please enter an email address.'),
-    email('Enter a valid email e.g yourname@gmail.com.'),
-    maxLength(256, 'Email too long. Enter an shorter alternative email.'),
+    minLength(1, "Please enter an email address."),
+    email("Enter a valid email e.g yourname@gmail.com."),
+    maxLength(256, "Email too long. Enter an shorter alternative email."),
   ]),
-  password: string([minLength(1, 'Please enter a password.')]),
-})
+  password: string([minLength(1, "Please enter a password.")]),
+});
 
 type LoginForm = Input<typeof LoginSchema>;
 
 export default component$(() => {
-  return <div>New route works.</div>
-})
+  const nav = useNavigate();
+
+  return <div>New route works.</div>;
+});
