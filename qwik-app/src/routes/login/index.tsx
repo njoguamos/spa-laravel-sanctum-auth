8599035cca999691cb71a4a6fbf6c8f607398705
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2024-02-10 17:27:29 +0300

    Add login form schema

--- qwik-app/src/routes/login/index.tsx
+++ qwik-app/src/routes/login/index.tsx
@@ -1,5 +1,21 @@
-import { component$ } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik"
+import type { Input} from "valibot"
+import {email, maxLength, minLength, object, string} from "valibot"
+
+// noinspection JSUnresolvedReference
+const endpoint = import.meta.env.PUBLIC_API_ENDPOINT
+
+const LoginSchema = object({
+  email: string([
+    minLength(1, 'Please enter an email address.'),
+    email('Enter a valid email e.g yourname@gmail.com.'),
+    maxLength(256, 'Email too long. Enter an shorter alternative email.'),
+  ]),
+  password: string([minLength(1, 'Please enter a password.')]),
+})
+
+type LoginForm = Input<typeof LoginSchema>;
 
 export default component$(() => {
-  return <div>New route works.</div>;
-});
+  return <div>New route works.</div>
+})
